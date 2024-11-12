8331bc687f82721d124fa62f4dfb199615d17cf3
prajapatkavitha <143082071+prajapatkavitha@users.noreply.github.com>
2024-11-12 15:42:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('.navbar ul li a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  
